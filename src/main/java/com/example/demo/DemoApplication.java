bf8ed07fe98fc1f5dcf9c57fc9da07fcd914a98f
Soumendra Daas <soumendra.daas@gmail.com>
2020-09-12 10:30:42 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/DemoConsumer.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.util.Map;
+
+@EnableBinding(Sink.class)
+@Slf4j
+public class DemoConsumer {
+
+    @StreamListener(Sink.INPUT)
+    public void handle(String msg){
+        log.info("DemoConsumer::handle() was called");
+        log.info(msg);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void logger(@Payload  String payload, @Headers Map<String,Object> headers){
+        log.info("DemoConsumer::logger called");
+        log.info("Payload : {}", payload);
+        log.info("Headers ...");
+        for( String key : headers.keySet()) {
+            log.info("{}: {}", key, headers.get(key));
+        }
+    }
+}
